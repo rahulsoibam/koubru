f42159aacec85cf1483464a6bf5052b86163adaf
Rahul Soibam <rahulsoibam@gmail.com>
2019-02-01 04:36:51 +0530

    uzz

--- apps/topics/queries.go
+++ apps/topics/queries.go
@@ -127,7 +127,7 @@ func (a *App) dbAuthenticatedGetTopicByID(userID int64, topicID int64) (*Topic, 
 		array_agg(c.name),
 		CASE WHEN EXISTS (SELECT 1 FROM topic_follower AS tf WHERE tf.topic_id = t.topic_id AND tf.followed_by=$1) THEN 1 ELSE 0 END AS is_following
 	FROM
-		Topic t INNER JOIN Topic_Category tc USING (topic_id) INNER JOIN Category c USING(category_id) INNER JOIN KUser as u ON t.created_by=u.user_id
+		Topic t LEFT JOIN Topic_Category tc USING (topic_id) LEFT JOIN Category c USING(category_id) LEFT JOIN KUser as u ON t.created_by=u.user_id
 	WHERE t.topic_id=$1
 	GROUP BY t.topic_id, u.user_id
 	`, topicID)
@@ -171,7 +171,7 @@ func (a *App) dbGetTopicByID(topicID int64) (*Topic, error) {
 		array_agg(c.category_id),
 		array_agg(c.name)
 	FROM
-		Topic t INNER JOIN Topic_Category tc USING (topic_id) INNER JOIN Category c USING(category_id) INNER JOIN KUser as u ON t.created_by=u.user_id
+		Topic t LEFT JOIN Topic_Category tc USING (topic_id) LEFT JOIN Category c USING(category_id) LEFT JOIN KUser as u ON t.created_by=u.user_id
 	WHERE t.topic_id=$1
 	GROUP BY t.topic_id, u.user_id
 	`, topicID)
