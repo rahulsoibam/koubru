4ba778461a9331e2d536a604b05593fae611bc56
Rahul Soibam <rahulsoibam@gmail.com>
2019-02-17 18:28:08 +0530

    added dev env variables

--- dev.sh
+++ dev.sh
@@ -1,27 +1,30 @@
 export API_PORT=':8080'
 # PostgreSQL related env variables
 # DB_HOST='koubru-dev-db.caawnufl2ajb.ap-south-1.rds.amazonaws.com'
-export DB_HOST='localhost'
+export DB_HOST='koubru-dev-db-cluster.cluster-caawnufl2ajb.ap-south-1.rds.amazonaws.com'
 # DB_USERNAME='rahulsoibam'
-export DB_USERNAME='etherealkatana'
+export DB_USERNAME='rahulsoibam'
 export DB_PASSWORD='058627439'
 export DB_NAME='koubru_prod'
 export DB_PORT='5432'
 
 # User credentials postgresql database for security
-export AUTH_DB_HOST='localhost'
-export AUTH_DB_USERNAME='etherealkatana'
+export AUTH_DB_HOST='koubru-dev-db-cluster.cluster-caawnufl2ajb.ap-south-1.rds.amazonaws.com'
+export AUTH_DB_USERNAME='rahulsoibam'
 export AUTH_DB_PASSWORD='058627439'
 export AUTH_DB_NAME='auth'
 export AUTH_DB_PORT='5432'
 
 # Credentials for redis instance for authentication
-export AUTH_REDIS_ADDRESS='localhost:6379'
+export AUTH_REDIS_ADDRESS='koubruauthredis-0001-001.qwpnzg.0001.aps1.cache.amazonaws.com:6379'
 export AUTH_REDIS_PASSWORD=''
 
+export CACHE_REDIS_ADDRESS='koubruauthredis-0001-002.qwpnzg.0001.aps1.cache.amazonaws.com'
+export CACHE_REDIS_PASSWORD=''
 # AWS related env variables
 export AWS_REGION='ap-south-1'
-export S3_BUCKET='media.rahulsoibam.me'
+export S3_BUCKET='ingest.koubru.app'
+export QUEUE_URL='https://sqs.ap-south-1.amazonaws.com/061205209873/koubru_video_transcode_output'
 
 # SendGrid Key
 export SENDGRID_API_KEY='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'
